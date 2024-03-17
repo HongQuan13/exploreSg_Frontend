166c7203d41c07d4c49017f05944c50c6886396d
Tran Que An <96300746+tquean15012003@users.noreply.github.com>
2024-03-17 15:18:30 +0800

    refactor login and register page

---
+++ src/webpages/access/hooks/useRegister.ts
@@ -0,0 +1,26 @@
+import { ILoginData } from "./useLogin";
+import { axiosClient } from "../../../libs/axios";
+import { useMutation } from "@tanstack/react-query";
+import { DefaultMutationOptions } from "../../../libs/react-query";
+
+import qs from "qs";
+
+export interface IRegisterData extends ILoginData {
+  username: string;
+}
+
+export const useRegister = (options: DefaultMutationOptions = {}) => {
+  return useMutation({
+    mutationKey: ["login"],
+    mutationFn: async (registerData: IRegisterData) => {
+      const registerResponse = await axiosClient.post(
+        `${process.env.REACT_APP_BASE_URL}/access/register`,
+        qs.stringify(registerData)
+      );
+
+      console.log(registerResponse, "loginResponse");
+      return registerResponse;
+    },
+    ...options,
+  });
+};
